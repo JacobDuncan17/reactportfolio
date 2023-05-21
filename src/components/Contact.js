9539397476e8d564a754f15620e7da21d3d8e271
Jacob Duncan <duncan@Jacobs-MacBook-Pro.local>
2023-05-20 19:22:14 -0600

    Finished react portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -16,7 +16,7 @@ const Contact = () => {
 
         <div className=" flex justify-center items-center">
           <form
-            action="https://getform.io/f/61c99527-2b15-42cf-9b55-ad37d2f7daa6"
+            action="https://getform.io/f/eaac0e9d-a908-4924-b003-71665d468c62"
             method="POST"
             className=" flex flex-col w-full md:w-1/2"
           >
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,20 +1,23 @@
 import React from "react";
-import project1 from "../assets/portfolio/placeholder.jpeg"
-import project2 from "../assets/portfolio/placeholder.jpeg"
-import project3 from "../assets/portfolio/placeholder.jpeg"
-import project4 from "../assets/portfolio/placeholder.jpeg"
-import project5 from "../assets/portfolio/placeholder.jpeg"
-import project6 from "../assets/portfolio/placeholder.jpeg"
+import project1 from "../assets/portfolio/TextEditor.png";
+import project2 from "../assets/portfolio/Wolfman-2.png";
+import project3 from "../assets/portfolio/placeholder2.webp";
+import project4 from "../assets/portfolio/Bazaar.png";
+import project5 from "../assets/portfolio/GroupProject1.png";
+import project6 from "../assets/portfolio/placeholder2.webp";
 
 const Portfolio = () => {
   const portfolios = [
     {
       id: 1,
       src: project1,
+      codeLink: "https://github.com/JacobDuncan17/Payback",
+      deployedLink: "https://payback-pwa.herokuapp.com/",
     },
     {
       id: 2,
       src: project2,
+      codeLink: "https://github.com/JacobDuncan17/Payback",
     },
     {
       id: 3,
@@ -23,10 +26,14 @@ const Portfolio = () => {
     {
       id: 4,
       src: project4,
+      codeLink: "https://github.com/RoboTom01/Hogar_Bazaar",
+      deployedLink: "https://hogar-bazaar.herokuapp.com/login",
     },
     {
       id: 5,
       src: project5,
+      codeLink: "https://github.com/JacobDuncan17/GroupProject-1",
+      deployedLink: "https://jacobduncan17.github.io/GroupProject-1/",
     },
     {
       id: 6,
@@ -48,17 +55,21 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, codeLink, deployedLink }) => (
             <div key={id} className="shadow-md bg-black rounded-lg">
-              <img
-                src={src}
-                alt=""
-                className="rounded-md duration-200 hover:scale-105"
-              />
+              <a href={deployedLink} target="_blank" rel="noopener noreferrer">
+                <img
+                  src={src}
+                  alt=""
+                  className="rounded-md duration-200 hover:scale-105"
+                />
+              </a>
               <div className="flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                  Code
-                </button>
+                <a href={codeLink} target="_blank" rel="noopener noreferrer">
+                  <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                    Code
+                  </button>
+                </a>
               </div>
             </div>
           ))}
